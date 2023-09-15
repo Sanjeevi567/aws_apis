8c1029a8d14a9407377672561c40fb9fb8993c35
Sanjuvi <sanjeevimani567@gmail.com>
2023-09-15 22:50:59 +0530

    Updated lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -27,4 +27,4 @@ mod sns_ops;
 pub use sns_ops::SnsOps;
 
 mod rekognition_ops;
-pub use rekognition_ops::{FaceDetails, RefImageType, RekognitionOps, TextDetect};
+pub use rekognition_ops::{FaceDetails, RekognitionOps, TextDetect};
