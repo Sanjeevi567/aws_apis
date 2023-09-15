03930690fe1d54f21bee72962c3d95c0f217bbe1
Sanjuvi <sanjeevimani567@gmail.com>
2023-09-16 00:06:16 +0530

    Updated Polly

--- src/aws_polly.rs
+++ src/aws_polly.rs
@@ -1,12 +1,15 @@
-use std::{fs::OpenOptions, io::Write};
+use std::{fs::OpenOptions, io::Write, time::SystemTime};
 
 use aws_config::SdkConfig;
 use aws_sdk_memorydb::primitives::DateTimeFormat;
 use aws_sdk_polly::{
     primitives::ByteStream,
-    types::{Engine, Gender, LanguageCode, OutputFormat, TaskStatus, TextType, VoiceId},
+    types::{
+        Engine, LanguageCode, OutputFormat, SynthesisTask, TaskStatus, TextType, Voice, VoiceId,
+    },
     Client as PollyClient,
 };
+use std::ops::Deref;
 
 use colored::Colorize;
 
@@ -63,7 +66,7 @@ impl PollyOps {
         )
     }
 
-    pub async fn start_synthesise_task(
+    pub async fn start_speech_synthesise_task(
         &self,
         engine: &str,
         voice_id: &str,
@@ -72,7 +75,7 @@ impl PollyOps {
         text_to_synthesize: &str,
         output_format: &str,
         bucket_name: &str,
-    ) -> SynthesizeTask {
+    ) {
         let config = self.get_config();
         let client = PollyClient::new(config);
 
@@ -95,32 +98,54 @@ impl PollyOps {
             .text(text_to_synthesize)
             .text_type(text_type_builder)
             .output_s3_bucket_name(bucket_name)
+            .output_s3_key_prefix("speech_synthesis_task_outputs/")
             .send()
             .await
-            .expect("Error while start synthesize task");
-        let mut synthesizetask = SynthesizeTask::default();
+            .expect("Error while start synthesize task\n");
+        println!(
+            "The key prefix in the provided bucket is set to 'speech_synthesis_task_outputs/'.
+        "
+        );
 
         let synthesize_info = output.synthesis_task;
         if let Some(synthesizeinfo) = synthesize_info {
-            let engine = synthesizeinfo.engine;
-            let task_status = synthesizeinfo.task_status;
-            let output_uri = synthesizeinfo.output_uri;
-            let output_format = synthesizeinfo.output_format;
-            let text_type = synthesizeinfo.text_type;
-            let voice_id = synthesizeinfo.voice_id;
-            let language_code = synthesizeinfo.language_code;
-            synthesizetask = SynthesizeTask::build_synthesizetask(
-                engine,
-                task_status,
-                output_uri,
-                output_format,
-                text_type,
-                voice_id,
-                language_code,
-            );
-        }
+            let task_id = synthesizeinfo.task_id;
+            if let Some(task_id) = task_id {
+                println!(
+                    "The Task ID for initiating speech synthesis tasks is as follows: {}\n",
+                    task_id.green().bold()
+                );
 
-        synthesizetask
+                let mut file = OpenOptions::new()
+                    .create(true)
+                    .read(true)
+                    .write(true)
+                    .open("task_id.txt")
+                    .expect("Error while creating file\n");
+                let buf = format!("The speech synthesis task with ID {task_id} has been initiated for the bucket named {bucket_name}");
+                match file.write_all(buf.as_bytes()) {
+                    Ok(_) => println!("The Task ID for the speech synthesis task has been successfully written to the current directory\n"),
+                    Err(_) => println!("Error while writing data\n")
+                }
+            }
+        }
+    }
+    pub async fn get_speech_sythesis_result(&self, task_id: &str) -> Option<SynthesizeTask> {
+        let config = self.get_config();
+        let client = PollyClient::new(config);
+        let output = client
+            .get_speech_synthesis_task()
+            .task_id(task_id)
+            .send()
+            .await
+            .expect("Error while getting speech synthesis outputs\n");
+
+        let mut synthesis_task: Option<SynthesizeTask> = None;
+        if let Some(task) = output.synthesis_task {
+            let type_ = SynthesizeTask::wrap(task);
+            synthesis_task = Some(type_);
+        }
+        synthesis_task
     }
 
     /// Returns a tuple consisting of two vectors: one containing options for [`voice ID`](https://docs.rs/aws-sdk-polly/latest/aws_sdk_polly/types/struct.Voice.html#method.id) and the other containing options for [`language code`](https://docs.aws.amazon.com/polly/latest/dg/API_StartSpeechSynthesisTask.html#polly-StartSpeechSynthesisTask-request-LanguageCode), based on the engine name provided
@@ -158,7 +183,6 @@ impl PollyOps {
 
         let output = client
             .list_speech_synthesis_tasks()
-            .max_results(5)
             .status(status_builder)
             .send()
             .await
@@ -208,59 +232,31 @@ impl PollyOps {
 
         if let Some(voices) = output.voices {
             voices.into_iter().for_each(|voice| {
-                let gender = voice.gender;
-                let voice_id = voice.id;
-                let language_code = voice.language_code;
-                let language_name = voice.language_name;
-                let voice_name = voice.name;
-                let supported_engines = voice.supported_engines;
-                vec_of_voices.push(DescribeVoices::build_describe_voices(
-                    gender,
-                    voice_id,
-                    language_code,
-                    language_name,
-                    voice_name,
-                    supported_engines,
-                ));
+                vec_of_voices.push(DescribeVoices::wrap(voice));
             });
         }
         vec_of_voices
     }
 }
 
-/// A struct for storing information of type [`SynthesisTask`](https://docs.rs/aws-sdk-polly/latest/aws_sdk_polly/types/struct.SynthesisTask.html#) which is returned from the [`start_synthesize_task`](https://docs.rs/aws-sdk-polly/latest/aws_sdk_polly/struct.Client.html#method.start_speech_synthesis_task) REST API
-#[derive(Default)]
-pub struct SynthesizeTask {
-    engine: Option<Engine>,
-    task_status: Option<TaskStatus>,
-    output_uri: Option<String>,
-    output_format: Option<OutputFormat>,
-    text_type: Option<TextType>,
-    voice_id: Option<VoiceId>,
-    language_code: Option<LanguageCode>,
+/// A wrapper struct for storing information of type [`SynthesisTask`](https://docs.rs/aws-sdk-polly/latest/aws_sdk_polly/types/struct.SynthesisTask.html#) which is returned from the [`start_synthesize_task`](https://docs.rs/aws-sdk-polly/latest/aws_sdk_polly/struct.Client.html#method.start_speech_synthesis_task) REST API
+pub struct SynthesizeTask(SynthesisTask);
+impl Deref for SynthesizeTask {
+    type Target = SynthesisTask;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
 }
 impl SynthesizeTask {
-    fn build_synthesizetask(
-        engine: Option<Engine>,
-        task_status: Option<TaskStatus>,
-        output_uri: Option<String>,
-        output_format: Option<OutputFormat>,
-        text_type: Option<TextType>,
-        voice_id: Option<VoiceId>,
-        language_code: Option<LanguageCode>,
-    ) -> Self {
-        Self {
-            engine,
-            task_status,
-            output_uri,
-            output_format,
-            text_type,
-            voice_id,
-            language_code,
-        }
+    pub fn wrap(type_: SynthesisTask) -> Self {
+        Self(type_)
+    }
+    pub fn get_task_id(&self) -> Option<&str> {
+        self.0.task_id()
     }
     pub fn get_engine(&self) -> Option<&str> {
-        let engine = if let Some(engine) = self.engine.as_ref() {
+        let engine = if let Some(engine) = self.0.engine.as_ref() {
             Some(engine.as_str())
         } else {
             None
@@ -269,7 +265,7 @@ impl SynthesizeTask {
     }
 
     pub fn get_task_status(&self) -> Option<&str> {
-        let task_status = if let Some(taskstatus) = self.task_status.as_ref() {
+        let task_status = if let Some(taskstatus) = self.0.task_status.as_ref() {
             Some(taskstatus.as_str())
         } else {
             None
@@ -278,7 +274,7 @@ impl SynthesizeTask {
     }
 
     pub fn get_output_uri(&self) -> Option<&str> {
-        let uri = if let Some(uri_) = self.output_uri.as_deref() {
+        let uri = if let Some(uri_) = self.0.output_uri.as_deref() {
             Some(uri_)
         } else {
             None
@@ -287,7 +283,7 @@ impl SynthesizeTask {
     }
 
     pub fn get_output_format(&self) -> Option<&str> {
-        let format = if let Some(format_) = self.output_format.as_ref() {
+        let format = if let Some(format_) = self.0.output_format.as_ref() {
             Some(format_.as_str())
         } else {
             None
@@ -296,7 +292,7 @@ impl SynthesizeTask {
     }
 
     pub fn get_text_type(&self) -> Option<&str> {
-        let text_type = if let Some(text) = self.text_type.as_ref() {
+        let text_type = if let Some(text) = self.0.text_type.as_ref() {
             Some(text.as_str())
         } else {
             None
@@ -305,7 +301,7 @@ impl SynthesizeTask {
     }
 
     pub fn get_voice_id(&self) -> Option<&str> {
-        let voice_id = if let Some(id) = self.voice_id.as_ref() {
+        let voice_id = if let Some(id) = self.0.voice_id.as_ref() {
             Some(id.as_str())
         } else {
             None
@@ -313,7 +309,7 @@ impl SynthesizeTask {
         voice_id
     }
     pub fn get_language_code(&self) -> Option<&str> {
-        let lang_code = if let Some(lang) = self.language_code.as_ref() {
+        let lang_code = if let Some(lang) = self.0.language_code.as_ref() {
             Some(lang.as_str())
         } else {
             None
@@ -321,77 +317,38 @@ impl SynthesizeTask {
         lang_code
     }
 }
-/// A struct for storing information of type [`Voice`](https://docs.rs/aws-sdk-polly/latest/aws_sdk_polly/types/struct.Voice.html) which is returned from the [`describe_voices`](https://docs.rs/aws-sdk-polly/latest/aws_sdk_polly/operation/describe_voices/builders/struct.DescribeVoicesFluentBuilder.html) REST API
-pub struct DescribeVoices {
-    gender: Option<Gender>,
-    voice_id: Option<VoiceId>,
-    language_code: Option<LanguageCode>,
-    language_name: Option<String>,
-    voice_name: Option<String>,
-    supported_engines: Option<Vec<Engine>>,
+/// A wrapper struct for storing information of type [`Voice`](https://docs.rs/aws-sdk-polly/latest/aws_sdk_polly/types/struct.Voice.html) which is returned from the [`describe_voices`](https://docs.rs/aws-sdk-polly/latest/aws_sdk_polly/operation/describe_voices/builders/struct.DescribeVoicesFluentBuilder.html) REST API
+pub struct DescribeVoices(Voice);
+
+impl Deref for DescribeVoices {
+    type Target = Voice;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
 }
 impl DescribeVoices {
-    fn build_describe_voices(
-        gender: Option<Gender>,
-        voice_id: Option<VoiceId>,
-        language_code: Option<LanguageCode>,
-        language_name: Option<String>,
-        voice_name: Option<String>,
-        supported_engines: Option<Vec<Engine>>,
-    ) -> Self {
-        Self {
-            gender,
-            voice_id,
-            language_code,
-            language_name,
-            voice_name,
-            supported_engines,
-        }
+    pub fn wrap(type_: Voice) -> Self {
+        Self(type_)
     }
     pub fn get_gender(&self) -> Option<&str> {
-        let gender = if let Some(gender) = self.gender.as_ref() {
-            Some(gender.as_str())
-        } else {
-            None
-        };
-        gender
+        self.0.gender().map(|gender| gender.as_str())
     }
     pub fn get_voiceid(&self) -> Option<&str> {
-        let voice_id = if let Some(voiceid) = self.voice_id.as_ref() {
-            Some(voiceid.as_str())
-        } else {
-            None
-        };
-
-        voice_id
+        self.0.id().map(|voice_id| voice_id.as_str())
     }
 
     pub fn get_language_code(&self) -> Option<&str> {
-        let language_code = if let Some(lang_code) = self.language_code.as_ref() {
-            Some(lang_code.as_str())
-        } else {
-            None
-        };
-        language_code
+        self.0.language_code().map(|code| code.as_str())
     }
     pub fn get_language_name(&self) -> Option<&str> {
-        let language_name = if let Some(lang_name) = self.language_name.as_ref() {
-            Some(lang_name.as_str())
-        } else {
-            None
-        };
-        language_name
+        self.0.language_name()
     }
     pub fn get_voice_name(&self) -> Option<&str> {
-        let voice_name = if let Some(voicename) = self.voice_name.as_ref() {
-            Some(voicename.as_str())
-        } else {
-            None
-        };
-        voice_name
+        self.0.name()
     }
     pub fn get_supported_engines(&self) -> Option<Vec<&str>> {
-        let vec_of_engines = if let Some(engines) = self.supported_engines.as_ref() {
+        let vec_of_engines = if let Some(engines) = self.0.supported_engines() {
             let mut vec_of_engine = Vec::new();
             engines.iter().for_each(|engines| {
                 vec_of_engine.push(engines.as_str());
