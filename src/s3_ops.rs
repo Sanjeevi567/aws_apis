ef68217fa466259e1f653e98968761ef3f6392ad
Sanjuvi <sanjeevimani567@gmail.com>
2023-10-15 10:51:08 +0530

    Updated S3

--- src/s3_ops.rs
+++ src/s3_ops.rs
@@ -22,17 +22,17 @@ use std::{
 /// these credentials are abstracted by this structure and its inherent functions
 /// and methods
 #[derive(Debug)]
-pub struct S3Ops {
-    config: SdkConfig,
+pub struct S3Ops<'a> {
+    config: &'a SdkConfig,
 }
-impl S3Ops {
+impl<'a> S3Ops<'a> {
     fn get_config(&self) -> &SdkConfig {
         &self.config
     }
 
     /// This function accepts an [`SdkConfig`](https://docs.rs/aws-config/latest/aws_config/struct.SdkConfig.html), retrieves the region name from it if
     /// available; otherwise, it sets it to an empty string and then constructs a S3Ops instance   
-    pub fn build(config: SdkConfig) -> Self {
+    pub fn build(config: &'a SdkConfig) -> Self {
         Self { config: config }
     }
 
